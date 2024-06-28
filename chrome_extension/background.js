37270190d9507febd14b6150a7d98c16348d73d5
Andy Tong <andytong369@gmail.com>
2024-06-28 18:18:17 -0400

    Update background.js

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,14 +1,15 @@
-function sendUrl(){
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        const url = tabs[0].url;
+function sendUrl() {
+    (async () => {
+        const [tab] = await chrome.tabs.query({active: true, lastFocusedWindow: true});
+        const url = tab.url;
         fetch('http://localhost:5000/receive_url', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ url: url })
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ url: url })
         });
-    });
+      })();
 }
 
 sendUrl();
