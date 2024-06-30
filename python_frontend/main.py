bbdf3c8330aef75989706bd8c09dbf6ee2c7fe63
Andy Tong <andytong369@gmail.com>
2024-06-30 19:10:49 -0400

    Update main.py

--- python_frontend/main.py
+++ python_frontend/main.py
@@ -86,7 +86,7 @@ def main():
         # Create GUI
 
         root = tk.Tk() # Shortcut
-        root.title("Sort Downloads by Website of Origin by Andy Tong")
+        root.title("WebSort Downloads by Andy Tong")
         root.geometry("500x270")
 
         # Create Tabs
